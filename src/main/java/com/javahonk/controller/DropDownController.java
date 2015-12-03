186e4d1602147d41dc61081ee6ae9bad46679231
dengwenxin <dengwenxin2143@163.com>
2015-12-03 17:48:14 +0800

    drps

--- src/main/java/com/javahonk/controller/DropDownController.java
+++ src/main/java/com/javahonk/controller/DropDownController.java
@@ -16,7 +16,7 @@ public class DropDownController {
 	@RequestMapping(value = "/dropDown.web", method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		
-		System.out.println("drops");
+		System.out.println("dropes");
 		return "dropDown";
 	}
 
